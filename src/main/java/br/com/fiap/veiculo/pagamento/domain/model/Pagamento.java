bfa7056bcaa3ff2fda8e6152e2f369d39436f79f
Wesley Junior de Souza <wesjuniorsouza@gmail.com>
2025-10-22 20:38:00 -0300

    inclusao modelo pagamento

---
+++ src/main/java/br/com/fiap/veiculo/pagamento/domain/model/Pagamento.java
@@ -0,0 +1,33 @@
+package br.com.fiap.veiculo.pagamento.domain.model;
+
+import br.com.fiap.veiculo.pagamento.presentation.enumerado.Status;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "pagamento")
+public class Pagamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "data_venda")
+    private LocalDateTime dataVenda;
+
+    @Column(name = "valor_remanescente")
+    private BigDecimal valorRemanescente;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(name = "codigo_pagamento")
+    private String codigoPagamento;
+}
---
+++ src/main/java/br/com/fiap/veiculo/pagamento/presentation/enumerado/Status.java
@@ -0,0 +1,7 @@
+package br.com.fiap.veiculo.pagamento.presentation.enumerado;
+
+public enum Status {
+
+    APROVADO,
+    RECUSADO
+}
