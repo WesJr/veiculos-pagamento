524c9c59e746f2860ca4a40a06280f7dce053fcc
Wesley Junior de Souza <wesjuniorsouza@gmail.com>
2025-10-30 00:51:27 -0300

    ajuste repository

--- src/main/java/br/com/fiap/veiculo/pagamento/infraestructure/repository/PagamentoRepository.java
+++ src/main/java/br/com/fiap/veiculo/pagamento/infraestructure/repository/PagamentoRepository.java
@@ -1,9 +1,11 @@
 package br.com.fiap.veiculo.pagamento.infraestructure.repository;
 
 import br.com.fiap.veiculo.pagamento.domain.model.Pagamento;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
+@Repository
+public interface PagamentoRepository extends CrudRepository<Pagamento, Long> {
 
     
 }
