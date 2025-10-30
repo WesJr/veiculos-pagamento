c422e22c4409f4a63604f7b3f2ebdda920816109
Wesley Junior de Souza <wesjuniorsouza@gmail.com>
2025-10-29 21:22:11 -0300

    inclusao de teste de endpoint

--- src/main/java/br/com/fiap/veiculo/pagamento/domain/service/PagamentoService.java
+++ src/main/java/br/com/fiap/veiculo/pagamento/domain/service/PagamentoService.java
@@ -13,45 +13,45 @@ import java.util.Objects;
 @Service
 public class PagamentoService {
 
-    private final PagamentoRepository repository;
-    private final PagamentoAssembler assembler;
-    private final VeiculoAssembler veiculoAssembler;
-    private final VeiculoService veiculoService;
-    private final ReservaService reservaService;
-
-    public PagamentoService(PagamentoRepository repository, PagamentoAssembler assembler, VeiculoAssembler veiculoAssembler, VeiculoService veiculoService, ReservaService reservaService) {
-
-        this.repository = repository;
-        this.assembler = assembler;
-        this.veiculoAssembler = veiculoAssembler;
-        this.veiculoService = veiculoService;
-        this.reservaService = reservaService;
-    }
-
-    public void incluirPagamento(PagamentoDTO pagamentoDto) {
-
-        Pagamento pagamento = assembler.dtoParaModelo(pagamentoDto);
-        ReservaDto reserva = reservaService.buscarReservaPorCodigo(pagamentoDto.codigoPagamento());
-
-        try {
-            if(Objects.nonNull(reserva)) {
-                pagamento.setStatus(Status.APROVADO);
-                repository.save(pagamento);
-
-                Veiculo veiculo = veiculoAssembler.dtoParaModelo(veiculoService.consultarPorId(reserva.veiculoDto().id()));
-                veiculo.setVendido(true);
-
-                veiculoService.inserirVeiculo(veiculoAssembler.modeloParaDto(veiculo));
-
-            } else {
-                pagamento.setStatus(Status.RECUSADO);
-                repository.save(pagamento);
-                throw new PagamentoException("O pagamento não pode ser realizado sem reserva posterior");
-            }
-        } catch (Exception e) {
-            throw new PagamentoException("O pagamento não foi realizado", e);
-        }
-
-
-    }
+//    private final PagamentoRepository repository;
+//    private final PagamentoAssembler assembler;
+//    private final VeiculoAssembler veiculoAssembler;
+//    private final VeiculoService veiculoService;
+//    private final ReservaService reservaService;
+//
+//    public PagamentoService(PagamentoRepository repository, PagamentoAssembler assembler, VeiculoAssembler veiculoAssembler, VeiculoService veiculoService, ReservaService reservaService) {
+//
+//        this.repository = repository;
+//        this.assembler = assembler;
+//        this.veiculoAssembler = veiculoAssembler;
+//        this.veiculoService = veiculoService;
+//        this.reservaService = reservaService;
+//    }
+//
+//    public void incluirPagamento(PagamentoDTO pagamentoDto) {
+//
+//        Pagamento pagamento = assembler.dtoParaModelo(pagamentoDto);
+//        ReservaDto reserva = reservaService.buscarReservaPorCodigo(pagamentoDto.codigoPagamento());
+//
+//        try {
+//            if(Objects.nonNull(reserva)) {
+//                pagamento.setStatus(Status.APROVADO);
+//                repository.save(pagamento);
+//
+//                Veiculo veiculo = veiculoAssembler.dtoParaModelo(veiculoService.consultarPorId(reserva.veiculoDto().id()));
+//                veiculo.setVendido(true);
+//
+//                veiculoService.inserirVeiculo(veiculoAssembler.modeloParaDto(veiculo));
+//
+//            } else {
+//                pagamento.setStatus(Status.RECUSADO);
+//                repository.save(pagamento);
+//                throw new PagamentoException("O pagamento não pode ser realizado sem reserva posterior");
+//            }
+//        } catch (Exception e) {
+//            throw new PagamentoException("O pagamento não foi realizado", e);
+//        }
+//
+//
+//    }
 }
--- src/main/java/br/com/fiap/veiculo/pagamento/presentation/controller/PagamentoController.java
+++ src/main/java/br/com/fiap/veiculo/pagamento/presentation/controller/PagamentoController.java
@@ -2,10 +2,7 @@ package br.com.fiap.veiculo.pagamento.presentation.controller;
 
 import br.com.fiap.veiculo.pagamento.domain.service.PagamentoService;
 import br.com.fiap.veiculo.pagamento.presentation.dto.PagamentoDTO;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pagamento")
@@ -20,7 +17,12 @@ public class PagamentoController {
     @PostMapping("/incluir")
     public void incluirPagamento(@RequestBody PagamentoDTO pagamentoDto) {
 
-        pagamentoService.incluirPagamento(pagamentoDto);
+//        pagamentoService.incluirPagamento(pagamentoDto);
+    }
+
+    @GetMapping("/sucesso")
+    public String getMensagemSucesso() {
+        return "SUCESSO!!!!";
     }
 
 }
